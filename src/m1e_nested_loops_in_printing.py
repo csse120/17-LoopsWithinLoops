eb039a8739922b02e7b731638d3b7d6922010e2b
DavidMutchler <mutchler@rose-hulman.edu>
2019-10-16 15:59:14 -0400

    Replaced the multiplication examples with examples that more to the core of what this example is to show, namely, how the loop variables work inside a nested loop.

--- src/m1e_nested_loops_in_printing.py
+++ src/m1e_nested_loops_in_printing.py
@@ -14,55 +14,45 @@ Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
 
 def main():
     """ Calls the other functions to demonstrate them. """
-    classic_example_1(3, 9)
-    classic_example_2(4)
+    classic_example_1(4, 7)
+    classic_example_2(6)
     classic_example_3(5, 9)
 
 
 def classic_example_1(n, m):
-    """
-    Prints a multiplication table for numbers from 1 to n multiplied
-    by numbers from 1 to m, where n and m are the given parameters.
-
-    Preconditions: n and m are positive integers.
-    """
+    """ Prints the loop variables of a loop within a loop. """
     # -------------------------------------------------------------------------
     # Classic nested-loops, type 1:
     #    The number of inner-loop iterations does NOT depend
     #    on the current value of the outer-loop variable.
     # -------------------------------------------------------------------------
     print()
-    print('-----------------------------------------------------------')
-    header = 'Complete multiplication table for 1 x 1 through'
-    print(header, n, 'x', m)
-    print('-----------------------------------------------------------')
+    print('-----------------------------')
+    print('Classic example 1:')
+    print('-----------------------------')
 
-    for i in range(n):
+    for k in range(n):
         for j in range(m):
-            print(i + 1, j + 1, '=', (i + 1) * (j + 1))
-        print()
+            print(k, j)
+        print('Completed the inner loop {} times'.format(k + 1))
 
 
 def classic_example_2(n):
-    """
-    Prints PART of a multiplication table.
-    Precondition: n is a positive integer.
-    """
+    """ Prints the loop variables of a loop within a loop. """
     # -------------------------------------------------------------------------
     # Classic nested-loops, type 2:
     #    The number of inner-loop iterations DOES depend
     #    on the current value of the outer-loop variable.
     # -------------------------------------------------------------------------
     print()
-    print('-----------------------------------------------------------')
-    header = 'Partial multiplication table for 1 x 1 through'
-    print(header, n, 'x', n)
-    print('-----------------------------------------------------------')
+    print('-----------------------------')
+    print('Classic example 2:')
+    print('-----------------------------')
 
-    for i in range(n):  # The ONLY difference from the previous
-        for j in range(i + 1):  # example is   i+1   on this line.
-            print(i + 1, j + 1, '=', (i + 1) * (j + 1))
-        print()
+    for k in range(n):  # The ONLY difference from the previous
+        for j in range(k + 1):  # example is   k+1   on this line.
+            print(k, j)
+        print('Completed the inner loop {} times'.format(k + 1))
 
 
 def classic_example_3(n, m):
@@ -79,16 +69,15 @@ def classic_example_3(n, m):
     # print statements on the same line, then go to the next line.
     # -------------------------------------------------------------------------
     print()
-    print('-----------------------------------------------------------')
-    header = 'Complete multiplication table for 1 x 1 through'
-    print(header, n, 'x', m)
-    print('but with just products shown')
-    print('-----------------------------------------------------------')
+    print('-----------------------------')
+    print('Classic example 1:')
+    print('-----------------------------')
 
-    for i in range(n):
+    for k in range(n):
+        print(k, end=':  ')
         for j in range(m):
-            print((i + 1) * (j + 1), end=' ')
-        print()
+            print(' ', j, end='')
+        print('Completed the inner loop {} times'.format(k + 1))
 
 
 # -----------------------------------------------------------------------------
